9a8f9a353e44ee960820ecc7d5699c52c42080cf
jako1001 <59346794+jako1001@users.noreply.github.com>
2023-09-18 21:20:28 -0500

    created an insert method on DatabaseHandler that takes in a df and table and inserts it into the table. Also created a view_table method to view all rows in a table

--- app/script.py
+++ app/script.py
@@ -2,10 +2,13 @@ from transform_data.Transform import Transform
 from database_operations.DatabaseHandler import DatabaseHandler
 
 db = DatabaseHandler("root", "root", "mysql", "3306", "userinfo")
-print(db.conn)
 
 csv = Transform("./csv/MOCK_DATA.csv", "csv")
 
 df = csv.csv_to_dataframe()
 
 db.create_table("users", df)
+
+db.insert(df, "users")
+
+db.view_table("users")
