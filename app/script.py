f9552204854e5f1609f848e0214db30e7f011486
jako1001 <59346794+jako1001@users.noreply.github.com>
2023-09-18 23:59:25 -0500

    added a unstructured_to_dataframe method to take an unstructured text file and output relevant data from it

--- app/script.py
+++ app/script.py
@@ -3,14 +3,14 @@ from database_handler.DatabaseHandler import DatabaseHandler
 
 db = DatabaseHandler("root", "root", "mysql", "3306", "userinfo")
 
-csv = Transform("./csv/MOCK_DATA.csv")
+txt = Transform("./unstructured/unstructured.txt").unstructured_to_dataframe()
 
-df = csv.csv_to_dataframe()
+# csv = Transform("./csv/MOCK_DATA.csv")
 
-db.create_table("users", df)
+# df = csv.csv_to_dataframe()
 
-csv.ppp()
+# db.create_table("users", df)
 
-db.insert(df, "users")
+# db.insert(df, "users")
 
-db.view_table("users")
+# db.view_table("users")
--- app/transform_data/Transform.py
+++ app/transform_data/Transform.py
@@ -1,11 +1,11 @@
+import re
 import pandas as pd
 from pandas import DataFrame
 
 
-class Transform():
+class Transform:
     def __init__(self, file_path) -> None:
         self.file_path = file_path
-
 
     def csv_to_dataframe(
         self,
@@ -20,4 +20,17 @@ class Transform():
             return df
 
         except:
-            return f"Something went wrong while reading the csv with file path ({self.file_path}). Is the file path correct?"ç
+            return f"Something went wrong while reading the csv with file path ({self.file_path}). Is the file path correct?"
+
+    def unstructured_to_dataframe(self) -> DataFrame | str:
+        with open(self.file_path, "r") as file:
+            text = file.read()
+
+            emails = re.findall(
+                r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b", text
+            )
+
+            phone_numbers = re.findall(r"\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}", text)
+
+            print("EMAILS: ", emails)
+            print("PHONE NUMBERS", phone_numbers)
