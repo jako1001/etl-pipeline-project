8a745a1c46c7986ada49ce4f77f4d133025c96c7
jako1001 <59346794+jako1001@users.noreply.github.com>
2023-09-18 22:15:35 -0500

    renamed database operations file

--- app/script.py
+++ app/script.py
@@ -1,13 +1,15 @@
 from transform_data.Transform import Transform
-from database_operations.DatabaseHandler import DatabaseHandler
+from database_handler.DatabaseHandler import DatabaseHandler
 
 db = DatabaseHandler("root", "root", "mysql", "3306", "userinfo")
 
-csv = Transform("./csv/MOCK_DATA.csv", "csv")
+csv = Transform("./csv/MOCK_DATA.csv")
 
 df = csv.csv_to_dataframe()
 
 db.create_table("users", df)
+
+csv.ppp()
 
 db.insert(df, "users")
 
--- app/transform_data/Transform.py
+++ app/transform_data/Transform.py
@@ -2,28 +2,24 @@ import pandas as pd
 from pandas import DataFrame
 
 
-class Transform:
-    def __init__(self, file_path, type) -> None:
+class Transform():
+    def __init__(self, file_path) -> None:
         self.file_path = file_path
-        self.type = type
+
+    def ppp(self):
+        print("COLUMN NAMES IN TRANSFORM: ", self.column_names)
 
     def csv_to_dataframe(
         self,
     ) -> DataFrame | str:
         """Converts csv file at a given directory into a pandas DataFrame"""
-        if self.type == "csv":
-            try:
-                df = pd.read_csv(self.file_path)
+        try:
+            df = pd.read_csv(self.file_path)
 
-                print(
-                    f"Successfully created DataFrame from the csv file located at: ({self.file_path})"
-                )
-                return df
+            print(
+                f"Successfully created DataFrame from the csv file located at: ({self.file_path})"
+            )
+            return df
 
-            except:
-                return f"Something went wrong while reading the csv with file path ({self.file_path}). Is the file path correct?"
-
-        return "File is not a csv."
-
-    # def dataframe_to_sql_format(self, dataframe, db_conn, column):
-    #     return dataframe.to_sql("users", con=db_conn, if_exists="append", index=False)
+        except:
+            return f"Something went wrong while reading the csv with file path ({self.file_path}). Is the file path correct?"
