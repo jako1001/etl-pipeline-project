0f82411c6a5074ec21ae04fe0ae570ab02d7f43d
jako1001 <59346794+jako1001@users.noreply.github.com>
2023-09-20 00:13:15 -0500

    implemented the script function to run from input commands in docker

--- app/script.py
+++ app/script.py
@@ -1,16 +1,38 @@
 from transform_data.Transform import Transform
 from database_handler.DatabaseHandler import DatabaseHandler
+import sys
 
-db = DatabaseHandler("root", "root", "mysql", "3306", "userinfo")
 
-df = Transform("./unstructured/unstructured.txt").unstructured_email_and_number_to_dataframe()
+def script(file_name, table_name) -> None:
+    db = DatabaseHandler("root", "root", "mysql", "3306", "userdata")
 
-# csv = Transform("./csv/MOCK_DATA.csv")
+    file_path = f"./external_data/{file_name}"
 
-# df = csv.csv_to_dataframe()
+    df = ""
+    structure_input = file_name[-3:]
 
-db.create_table("users", df)
+    if structure_input == "csv":
+        df = Transform(file_path).csv_to_dataframe()
 
-db.insert(df, "users")
+    else:
+        df = Transform(file_path).unstructured_email_and_number_to_dataframe()
 
-db.view_table("users")
+    print("Creating database table...")
+    db.create_table(table_name, df)
+
+    print("Inserting data into table...")
+    db.insert(df, table_name)
+
+    print("Done! Here is the data in your database!: ")
+    db.view_table(table_name)
+
+    db.conn.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 script.py <file_name> <table_name>")
+        sys.exit(1)
+
+    file_name = sys.argv[1]
+    table_name = sys.argv[2]
+    script(file_name , table_name)
